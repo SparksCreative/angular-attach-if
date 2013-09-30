7176eb46000b1e99d0b98dd6ef310780f9fc2e54
Simon Sparks <simon@sparks.uk.net>
2013-09-30 17:22:21 +0100

    Added the attach-if directive, example usage in a demo page, bower configuration and initial documentation.

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ angular-attach-if
 =================
 
 Like ng-if but with maintained scope.
+
+This simple directive may be used in place of the angular ngIf (or earlier angular-ui uiIf) directive in cases where
+conditionally detaching and re-attaching components without re-initialising scope / controller state would be helpful.
+
+When a child element is removed from the DOM tree, it's associated scope is taken out of the digest chain but not
+destroyed. If the child element is later returned to the DOM, it's scope object is wired back in and child controllers
+ may resume with their existing state.
---
+++ attach-if.js
@@ -0,0 +1,64 @@
+/*
+ * attach-if v0.2.0
+ * (c) 2013 Sparks Creative Limited http://sparks.uk.net
+ * License: MIT
+ */
+
+'use strict';
+
+angular.module('sparkscreative.attach-if', [])
+
+    /*
+     * Variation of the uiIf / ngIf directive with maintained scope.
+     * Allows detachment and reattachment of components without reinitialising controller state.
+     */
+    .directive('attachIf', [function () {
+        return {
+            restrict: 'A',
+            transclude: 'element',
+            priority: 1000,
+            terminal: true,
+            compile: function (element, attr, transclude) {
+                return function (scope, element, attr) {
+
+                    var childElement,
+                        childScope;
+
+                    scope.$watch(attr.attachIf, function (attach) {
+                        if (attach) {
+                            if(childScope) {
+                                childScope.$broadcast('attach');
+                                childScope.$parent = scope;
+                                scope.$$childHead = scope.$$childTail = childScope;
+                            } else {
+                                childScope = scope.$new();
+                            }
+
+                            if(childElement) {
+                                element.after(childElement);
+                            } else {
+                                transclude(childScope, function (clone) {
+                                    childElement = clone;
+                                    element.after(clone);
+                                });
+                            }
+                        } else {
+                            if(childScope) {
+                                childScope.$broadcast('detach');
+                                scope.$$childHead = scope.$$childTail = null;
+                                childScope.$parent = null;
+                            }
+
+                            if(childElement) {
+                                for (var elem, i = 0; (elem = childElement[i]); i++) {
+                                    if (elem.parentNode) {
+                                        elem.parentNode.removeChild(elem);
+                                    }
+                                }
+                            }
+                        }
+                    });
+                };
+            }
+        };
+    }]);
